c236ff6de8ec71f694328832646e8ee51ac23ac7
Pa-Par <parichatt2947@gmail.com>
2025-12-26 08:58:50 +0700

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const { sequelize } = require('./models')
+const config = require('./config/config')
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+// Import routes
+require('./routes')(app);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(config.port, () => {
+        console.log('Server running on port ' + config.port)
+    })
+})
